0830c51a42f20fd084ce7c33d25dcaba59e23f73
artsokol <artsokol87@gmail.com>
2017-01-28 21:32:59 +0300

    Implement base of node_entry
    The node_entry for navigable small world is significanlry differs
    from analogue in dht. It matters to clone most of functions with
    small changes. Most likely we should return later to this file
    and change it by adding more inforamtion.

--- include/libtorrent/navigable_small_world/node_entry.hpp
+++ include/libtorrent/navigable_small_world/node_entry.hpp
@@ -2,45 +2,65 @@
 #define NSW_NODE_ENTRY_HPP
 
 #include "libtorrent/navigable_small_world/node_id.hpp"
-//#include "libtorrent/navigable_small_world/term_vector.hpp"
 #include "libtorrent/socket.hpp"
 #include "libtorrent/address.hpp"
 #include "libtorrent/union_endpoint.hpp"
 #include "libtorrent/time.hpp" // for time_point
 
+#include <string>
+
 namespace libtorrent { namespace nsw
 {
 
+// The node_entry structure represents torrent file on navigable small world map
+// we will use this as item in our routing table to keep all necessary
+// information about remote nodes.
+
 struct TORRENT_EXTRA_EXPORT node_entry
 {
-	node_entry(node_id const& id_, udp::endpoint ep, int roundtriptime = 0xffff
-		, bool pinged = false);
-	node_entry(udp::endpoint ep);
-	node_entry();
-	void update_rtt(int new_rtt);
+    node_entry(node_id const& id_,
+        udp::endpoint const& ep,
+        int roundtriptime = ~0,
+        bool pinged = false,
+        std::string const& description = "");
 
-	bool pinged() const { return timeout_count != 0xff; }
-	void set_pinged() { if (timeout_count == 0xff) timeout_count = 0; }
-	void timed_out() { if (pinged() && timeout_count < 0xfe) ++timeout_count; }
-	int fail_count() const { return pinged() ? timeout_count : 0; }
-	void reset_fail_count() { if (pinged()) timeout_count = 0; }
-	udp::endpoint ep() const { return udp::endpoint(address_v4(a), p); }
-	bool confirmed() const { return timeout_count == 0; }
-	address addr() const { return address_v4(a); }
-	int port() const { return p; }
+    explicit node_entry(udp::endpoint const& ep);
+    node_entry();
 
+    void update_rtt(int new_rtt);
+
+    bool pinged() const { return timeout_count != ~0; }
+    void set_pinged() { if (timeout_count == ~0) timeout_count = 0; }
+    void timed_out() { if (pinged() && timeout_count < 0xfe) ++timeout_count; }
+    int fail_count() const { return pinged() ? timeout_count : 0; }
+    void reset_fail_count() { if (pinged()) timeout_count = 0; }
+
+    bool confirmed() const { return timeout_count == 0; }
+
+    //Just shortcuts
+    address addr() const { return endpoint.address(); }
+    int port() const { return endpoint.port; }
+
+    // the time we last received a response for a request to this peer
 	time_point last_queried;
 
 	node_id id;
 
-    //nsw::term_vector term;
+    //i think it makes sence to keep term vector of all friends
+    std::string term_vector;
 
-	address_v4::bytes_type a;
-	boost::uint16_t p;
+    union_endpoint endpoint;
 
-	boost::uint16_t rtt;
+    // the average RTT of this node
+	std::uint16_t rtt;
 
-	boost::uint8_t timeout_count;
+    // the number of times this node has failed to
+    // respond in a row
+	std::uint8_t timeout_count;
+
+    #ifndef TORRENT_DISABLE_LOGGING
+    time_point first_seen;
+    #endif
 };
 
 } }
--- src/navigable_small_world/node_entry.cpp
+++ src/navigable_small_world/node_entry.cpp
@@ -5,42 +5,44 @@ namespace libtorrent {
 namespace nsw
 {
 
-	node_entry::node_entry(node_id const& id_, udp::endpoint ep
+	node_entry::node_entry(node_id const& id_, udp::endpoint const& ep
 		, int roundtriptime
-		, bool pinged)
+		, bool pinged
+		, std::string const& description)
 		: last_queried(pinged ? aux::time_now() : min_time())
 		, id(id_)
-//		, term()
-		, a(ep.address().to_v4().to_bytes())
-		, p(ep.port())
-		, rtt(roundtriptime & 0xffff)
-		, timeout_count(pinged ? 0 : 0xff)
-	{
+		, term_vector(description)
+		, endpoint(ep)
+		, rtt(roundtriptime & ~0)
+		, timeout_count(pinged ? 0 : ~0)
+#ifndef TORRENT_DISABLE_LOGGING
+		, first_seen(aux::time_now())
+#endif
+	{}
 
-	}
-
-	node_entry::node_entry(udp::endpoint ep)
+	node_entry::node_entry(udp::endpoint const& ep)
 		: last_queried(min_time())
-		, id(0)
-//	    , term()
-		, a(ep.address().to_v4().to_bytes())
-		, p(ep.port())
-		, rtt(0xffff)
-		, timeout_count(0xff)
-	{
-
-	}
+		, id(nullptr)
+		, term_vector("")
+		, endpoint(ep)
+		, rtt(~0)
+		, timeout_count(~0)
+#ifndef TORRENT_DISABLE_LOGGING
+		, first_seen(aux::time_now())
+#endif
+	{}
 
 	node_entry::node_entry()
 		: last_queried(min_time())
-		, id(0)
-//		, term()
-		, p(0)
-		, rtt(0xffff)
-		, timeout_count(0xff)
-	{
+		, id(nullptr)
+		, term_vector("")
+		, rtt(~0)
+		, timeout_count(~0)
+#ifndef TORRENT_DISABLE_LOGGING
+		, first_seen(aux::time_now())
+#endif
+	{}
 
-	}
 
 	void node_entry::update_rtt(int new_rtt)
 	{
