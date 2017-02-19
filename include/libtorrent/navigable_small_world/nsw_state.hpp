4633893b65b3f0ae5ec909f2251e2fb7d2bca725
artsokol <artsokol87@gmail.com>
2017-02-19 16:34:39 +0300

    Implement nsw_state class.
    Code incommon copied from kedemli with small changes.

--- include/libtorrent/navigable_small_world/nsw_state.hpp
+++ include/libtorrent/navigable_small_world/nsw_state.hpp
@@ -17,19 +17,33 @@ namespace libtorrent
 namespace libtorrent {
 namespace nsw
 {
-	struct TORRENT_EXPORT nsw_state
+	// This structure helps to store and load the state
+	// of the ``nsw_tracker``.
+	// supports both IPv4 and IPv6 at the same time for
+	class TORRENT_EXPORT nsw_state
 	{
+	public:
+		// the id of the IPv4 node
 		node_id nid;
+#if TORRENT_USE_IPV6
+		// the id of the IPv6 node
 		node_id nid6;
-
+#endif
+		// the bootstrap nodes saved from the IPv4 buckets node
 		std::vector<udp::endpoint> nodes;
+#if TORRENT_USE_IPV6
+		// the bootstrap nodes saved from the IPv6 buckets node
 		std::vector<udp::endpoint> nodes6;
-
+#endif
 		void clear();
+
+		TORRENT_EXTRA_EXPORT void read_nsw_state(bdecode_node const& e,nsw_state& ret_state);
+		TORRENT_EXTRA_EXPORT entry save_nsw_state(nsw_state const& state);
+	private:
+		node_id extract_node_id(bdecode_node const& e, string_view key);
+		entry save_nodes(std::vector<udp::endpoint> const& nodes);
 	};
 
-	TORRENT_EXTRA_EXPORT nsw_state read_nsw_state(bdecode_node const& e);
-	TORRENT_EXTRA_EXPORT entry save_nsw_state(nsw_state const& state);
 }}
 
 #endif // LIBTORRENT_NSW_STATE_HPP
--- src/navigable_small_world/nsw_state.cpp
+++ src/navigable_small_world/nsw_state.cpp
@@ -6,36 +6,75 @@
 namespace libtorrent {
 namespace nsw
 {
-namespace
-{
-	node_id extract_node_id(bdecode_node const& e, string_view key)
+	inline node_id nsw_state::extract_node_id(bdecode_node const& e, string_view key)
 	{
+		if (e.type() != bdecode_node::dict_t) return node_id();
+
 		auto nid = e.dict_find_string_value(key);
+
+		if (nid.size() != 20) return node_id();
+
 		return node_id(nid);
 	}
 
-	entry save_nodes(std::vector<udp::endpoint> const& nodes)
+	// returns entry object with serialized nodes
+	entry nsw_state::save_nodes(std::vector<udp::endpoint> const& nodes)
 	{
-		entry ret;
+		entry ret(entry::list_t);
+		entry::list_type& list = ret.list();
+
+		for (auto const& ep : nodes)
+		{
+			std::string node;
+			std::back_insert_iterator<std::string> out(node);
+			detail::write_endpoint(ep, out);
+			list.push_back(entry(node));
+		}
 		return ret;
 	}
-} // anonymous namespace
 
-	void nsw_state::clear()
+
+	inline void nsw_state::clear()
 	{
+		nid.clear();
+		nid6.clear();
+
+		nodes.clear();
+		nodes.shrink_to_fit();
+		nodes6.clear();
+		nodes6.shrink_to_fit();
 	}
 
-	nsw_state read_nsw_state(bdecode_node const& e)
+	void nsw_state::read_nsw_state(bdecode_node const& e, nsw_state& ret)
 	{
-		(void)e;
-		nsw_state ret;
-		return ret;
+		if (e.type() != bdecode_node::dict_t) return;
+
+		ret.nid = extract_node_id(e, "node-id");
+#if TORRENT_USE_IPV6
+		ret.nid6 = extract_node_id(e, "node-id6");
+#endif
+
+		if (bdecode_node const nodes = e.dict_find_list("nodes"))
+			ret.nodes = detail::read_endpoint_list<udp::endpoint>(nodes);
+#if TORRENT_USE_IPV6
+		if (bdecode_node const nodes = e.dict_find_list("nodes6"))
+			ret.nodes6 = detail::read_endpoint_list<udp::endpoint>(nodes);
+#endif
+
+//		return ret;
 	}
 
-	entry save_nsw_state(nsw_state const& state)
+	entry nsw_state::save_nsw_state(nsw_state const& state)
 	{
-		(void)state;
-		entry ret;
+		entry ret(entry::dictionary_t);
+		ret["node-id"] = state.nid.to_string();
+		entry const nodes = save_nodes(state.nodes);
+		if (!nodes.list().empty()) ret["nodes"] = nodes;
+#if TORRENT_USE_IPV6
+		ret["node-id6"] = state.nid6.to_string();
+		entry const nodes6 = save_nodes(state.nodes6);
+		if (!nodes6.list().empty()) ret["nodes6"] = nodes6;
+#endif
 		return ret;
 	}
 }}
